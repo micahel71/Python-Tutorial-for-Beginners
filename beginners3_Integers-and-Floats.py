fb8be635472950e7677cf2c0b96b53bab00e05ff
Michael Chalvatzis <michael.chalvatzis@ing-diba.de>
2018-11-21 21:33:51 +0100

    further classes

---
+++ beginners3_Integers-and-Floats.py
@@ -0,0 +1,66 @@
+num = 3
+num1 = 3.14
+
+print (type(num))
+print (type(num1))
+
+# Arithmetic Operator:
+# Addition:         3 + 2
+# Subtraction:      3 - 2
+# Multiplication:   3 * 2
+# Division:         3 / 2
+# Floor Division:   3 // 2
+# Exponent:         3 ** 2
+# Modulus:          3 % 2
+
+print('3+2=',3+2)
+print('3-2=',3-2)
+print(3*2)
+print(3/2)
+print(3//2)
+print(3**2)
+print(2%2)
+print(3%2)
+print(4%2)
+print(5%2)
+print(3*2+1)
+print(3*(2+1))
+
+num = 1
+num = num + 1
+num += 1
+num *= 1
+num *= 10
+print(num)
+
+print(abs(-3))
+print(round(3.75))
+print(round(3.75,1))
+
+# Comparisons
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater than:     3 > 2
+# Less than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
+
+num_1 = 3
+num_2 = 2
+
+print (num_1 == num_2)
+print (num_1 != num_2)
+print (num_1 > num_2)
+print (num_1 < num_2)
+print (num_1 >= num_2)
+print (num_1 <= num_2)
+
+num_3 = '100'
+num_4 = '200'
+print(num_3 + num_4)
+
+num_3 = '100'
+num_4 = '200'
+num_3 = int(num_3)
+num_4 = int(num_4)
+print(num_3 + num_4)
